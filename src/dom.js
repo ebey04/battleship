564bb486cdb3734b5eb0c7c1910112b31d961f05
Elizabeth Bey <ebey04@gmail.com>
2025-12-10 14:52:46 -0500

    feat(dom) create gridCreation() and build #startGameBtn event listener

--- src/dom.js
+++ src/dom.js
@@ -7,3 +7,23 @@ const enemyBoard   = document.getElementById('enemy-board');
 const overlay      = document.getElementById('overlay');
 const startGameBtn = document.getElementById('start-game');
 const messages     = document.getElementById('messages');
+
+
+function gridCreation(boardElement) {
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) { 
+            const div = document.createElement('div'); 
+            div.classList.add('cell'); 
+            div.dataset.row = i
+            div.dataset.col = j
+            boardElement.appendChild(div);
+            }
+        }
+    }
+
+startGameBtn.addEventListener("click", () => {
+    overlay.classList.add("hidden");
+    gridCreation(fleetBoard)
+    gridCreation(enemyBoard)
+    deployBtn.classList.remove("hidden");
+})
